b026fcf43d605d8f009200af8f570d927762fd72
ahsan sayeed <ahsansayeedsaad@gmail.com>
2022-09-03 12:37:13 +0100

    modal added

--- script/index,.js
+++ script/index,.js
@@ -1,5 +1,6 @@
 const catagoryUrl = 'https://openapi.programming-hero.com/api/news/categories';
 const newsUrl = 'https://openapi.programming-hero.com/api/news/category/';
+const newsDetailsUrl = 'https://openapi.programming-hero.com/api/news/';
 const catagoryContainer = document.getElementById('catagoryContainer');
 const newsContainer = document.getElementById('newsContainer');
 const itemFound = document.getElementById('itemFound');
@@ -14,14 +15,13 @@ fetch(catagoryUrl)
 .catch(e=>console.log(e));
 
 function getData(catagoryId,catagoryName){
+    document.getElementById('spinner').classList.remove('d-none');
     newsContainer.innerHTML="";
     fetch(newsUrl+catagoryId)
     .then(e=>e.json())
     .then(e=>{
-        // console.log(catagoryName);
         itemFound.innerText = e.data.length!==0?`${e.data.length} items found for category ${catagoryName}`:`No items found for catagory ${catagoryName}`;
         e.data.forEach(v=>{
-            // console.log(v)
             newsContainer.innerHTML += `
             <div class="card mb-4 border-0" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.204), 0 6px 50px 0 rgba(0, 0, 0, 0.073);">
             <div class="card-body row g-1">
@@ -33,13 +33,13 @@ function getData(catagoryId,catagoryName){
                     <div class="col d-flex align-items-center">
                       <img src="${v.author.img}" alt="" style="height:40px;width:40px;border-radius:100%">
                       <div class="d-flex flex-column justify-content-center ms-2">
-                        <span style="font-size:14px;font-weight: 500;white-space: nowrap;">${v.author.name}</span>
-                        <span style="font-size:12px; white-space;nowrap;">${v.author.published_date}</span>
+                        <span style="font-size:14px;font-weight: 500;white-space: nowrap;">${v.author.name===null||v.author.name===''?'No data available':v.author.name}</span>
+                        <span style="font-size:12px; white-space;nowrap;">${v.author.published_date===null?"No data available":v.author.published_date}</span>
                       </div>
                     </div>
                     <div class="col d-flex align-items-center  justify-content-lg-start justify-content-end">
                       <i class="fa-regular fa-eye"></i>&nbsp
-                      <span>${v.total_view}</span>
+                      <span>${v.total_view===null?"No data available":v.total_view}</span>
                     </div>
                     <div class="col-12 col-lg-3 mt-3 mt-lg-0 d-flex align-items-center justify-content-start">
                       <i class="fa-sharp fa-solid fa-star-half-stroke"></i>
@@ -49,12 +49,66 @@ function getData(catagoryId,catagoryName){
                       <i class="fa-regular fa-star"></i>
                     </div>
                     <div class="col-12 col-lg-3 d-flex align-items-center justify-content-end">
-                      <i class="fa-solid fa-arrow-right text-primary"></i>
+                    
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn border border-0" data-toggle="modal" data-target="#exampleModalCenter" onclick="newsDetails('${v._id}')">
+                    <i class="fa-solid fa-arrow-right text-primary"></i>
+                    </button>
+                
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                      <div class="modal-dialog modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                          <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLongTitle">d...dynamic title</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                            </button>
+                          </div>
+                          <div class="modal-body">
+                            <div class="d-flex">
+                                <img src="" id='authorImage' class='ms-2 me-4' style="height:60px;width:60px;border-radius:100%">
+                                <div style='line-height:0'>
+                                    <h6 class='mt-2' id="authorName">author name</h6>
+                                    <span style="font-size:12px;" id="autorTimeData">Time and date</span>
+                                </div>
+                            </div>
+                
+                            <img src="" id="modalImage" class="img-fluid my-5">
+                            <div id="modalBody">
+                            ...dynamic body
+                            </div>
+                          </div>
+                          <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
                     </div>
                   </div>
               </div>
             </div>
           </div>`;
+        });
+        document.getElementById('spinner').classList.add('d-none');
+    })
+    .catch(e=>console.log(e));
+}
+
+function newsDetails(id){
+    fetch(newsDetailsUrl+id)
+    .then(e=>e.json())
+    .then(e=>{
+        e.data.forEach((value)=>{
+            document.getElementById('exampleModalLongTitle').innerText = value.title;
+            document.getElementById('modalImage').src = value.image_url;
+            document.getElementById('modalBody').innerText = value.details;
+            document.getElementById('authorImage').src= value.author.img;
+            document.getElementById('authorName').innerText = value.author.name===null||value.author.name===''?"No data available":value.author.name;
+            document.getElementById('autorTimeData').innerText = value.author.published_date===null?"No data available":value.author.published_date;
+            console.log(value.img)
         });
     })
     .catch(e=>console.log(e));
